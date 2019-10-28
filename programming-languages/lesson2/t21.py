2b96e4d377420d4b03bb05c2027ba80c1c59962f
Aleksei <piskunov.alesha@gmail.com>
2019-10-29 00:50:17 +0700

    Многопоточность (#1)
    Многопоточность (ЯП 2 урок)

---
+++ programming-languages/lesson2/t21.py
@@ -0,0 +1,81 @@
+from threading import Thread, Condition
+import random
+import time
+import signal
+
+
+threads = list()
+
+
+class MyCondition(Condition):
+    useful_meta = None
+
+
+class SimpleThread(Thread):
+    def __init__(self, name, condition: MyCondition):
+        Thread.__init__(self, name=name, target=self._target)
+        self.condition = condition
+
+    def run(self):
+        try:
+            self._target()
+        finally:
+            with self.condition:
+                print(f'Thread {self.getName()} exit')
+                self.condition.useful_meta = -1
+                self.condition.notify_all()
+                del self._target
+
+    def _target(self):
+        while True:
+            with self.condition:
+                while self.condition.useful_meta != self.getName():
+                    if self.condition.useful_meta == -1:
+                        return
+                    self.condition.wait()
+                print(f'Thread {self.getName()}')
+                self.condition.useful_meta = None
+
+
+class MainThread(Thread):
+    def __init__(self, count: int, condition: MyCondition):
+        Thread.__init__(self, target=self._target)
+        self.condition = condition
+        self.count = count
+
+    def _target(self):
+        while True:
+            with self.condition:
+                cur = 1 + random.randrange(self.count)
+                self.condition.useful_meta = str(cur)
+                self.condition.notify_all()
+            time.sleep(2)
+
+    def close(self):
+        with self.condition:
+            self.condition.useful_meta = -1
+            self.condition.notify_all()
+        for t in threads:
+            t.join()
+        time.sleep(0.1)
+
+
+def main():
+    n = int(input('Input count of threads: '))
+    cond = MyCondition()
+
+    main_thread = MainThread(count=n, condition=cond)
+
+    for name in map(str, range(1, n + 1)):
+        threads.append(SimpleThread(name=name, condition=cond))
+        threads[-1].daemon = True
+        threads[-1].start()
+    try:
+        main_thread.run()
+        signal.pause()
+    except (KeyboardInterrupt, SystemExit):
+        main_thread.close()
+
+
+if __name__ == '__main__':
+    main()
---
+++ programming-languages/lesson2/t22.py
@@ -0,0 +1,88 @@
+import asyncio
+from timeit import timeit
+from random import randint
+from pprint import pprint
+
+
+def get_array():
+    constraint = (-2147483648, 2147483647)
+    count = int(input('Input size of array: '))
+    return [randint(*constraint) for _ in range(count)]
+
+
+class Collector:
+    response = None
+    results = {
+        'minimum': None,
+        'maximum': None,
+    }
+
+    def callback(self, response: asyncio.Future):
+        self.response = response.result()
+        self.resolve('minimum')
+        self.resolve('maximum')
+
+    def resolve(self, name):
+        if not self.results[name]:
+            self.results[name] = self.response[name]
+        if self.results[name][0] > self.response[name][0]:
+            self.results[name] = self.response[name]
+
+
+class Executor:
+    loop = None
+    collector = Collector()
+
+    def __init__(self, array):
+        self.array = array
+
+    def search(self, tasks: int):
+        self.loop = asyncio.get_event_loop()
+        datas = [self.data(code, tasks) for code in range(tasks)]
+        futures = []
+
+        for future, params in datas:
+            self.loop.create_task(self.task(future, *params))
+            futures.append(future)
+
+        self.loop.run_until_complete(asyncio.wait(futures))
+
+    def data(self, code, tasks):
+        fut = self.loop.create_future()
+        fut.add_done_callback(self.collector.callback)
+        return fut, self.borders(code, tasks)
+
+    def borders(self, code, tasks):
+        size = len(self.array) // tasks
+        return code * size, (
+            (code + 1) * size,
+            len(self.array),
+        )[code + 1 == tasks]
+
+    async def task(self, fut: asyncio.Future, left, right):
+        setup = lambda pos: (self.array[pos], pos)
+        minimum = maximum = setup(left)
+
+        for num in range(left, right):
+            if minimum[0] > self.array[num]:
+                minimum = setup(num)
+            if maximum[0] < self.array[num]:
+                maximum = setup(num)
+
+        return fut.set_result({
+            'minimum': minimum,
+            'maximum': maximum,
+        })
+
+
+if __name__ == '__main__':
+    results = []
+    exe = Executor(get_array())
+    for times in [2, 4, 8, 16]:
+        print(timeit(
+            f'exe.search({times})',
+            'from __main__ import exe',
+            number=100,
+        ), f'with {times} tasks')
+        results.append(exe.collector.results)
+    pprint(results)
